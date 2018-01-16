eb1328949ff81117123d8d511293cf4c4881bbb7
Saurav Sahu <sauravsahu.12@live.com>
2018-01-16 10:20:15 +0530

    Test passed for doubleAll function with for-loop

---
+++ doubleAll.js
@@ -0,0 +1,9 @@
+function doubleAll(numbers) {
+    let result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        result.push(numbers[i] * 2);
+    }
+    return result;
+}
+
+module.exports = doubleAll
---
+++ index.js
@@ -0,0 +1,17 @@
+let doubleAll = require('./doubleAll');
+
+function compareArrays(firstArray, secondArray) {
+    if (firstArray.length !== secondArray.length)
+        return false;
+    for (let i = 0; i < firstArray.length; ++i) {
+        if (firstArray[i] !== secondArray[i])
+            return false;
+    }
+    return true;
+}
+
+console.log('Test compareArray with two equal arrays: ', compareArrays([1, 2, 3], [1, 2, 3]) === true);
+console.log('Test compareArray with two unequal arrays: ', compareArrays([1, 2, 3, 4], [1, 2, 3]) === false);
+
+
+console.log('Test for doubling array elements containing only positive integers:', compareArrays(doubleAll([1, 2, 3]), [2, 4, 6]) === true);
